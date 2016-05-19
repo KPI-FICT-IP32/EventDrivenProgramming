f2febb6979276bc36c6a0d38cc69d19e9bfa603c
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-05-19 11:36:56 +0300

    Fix observable.js
    - Fix class method definitions
    - Add some testing-purpose rows
    - Add vim line

--- LiveTable/observable.js
+++ LiveTable/observable.js
@@ -5,17 +5,25 @@ class Observable {
         this.subscribers = [];
     }
 
-    function filter(fn) {
+    filter(fn) {
         const filtered = new Observable();
         console.log(`Observable.filter(${fn});`); 
         return filtered;
     }
 
-    function on(callback) {
+    on(callback) {
         console.log(`Observable.on(${callback});`);
     }
 
-    function emit(data) {
+    emit(data) {
         console.log(`Observable.emit(${data});`);
     }
 }
+
+
+var o = new Observable();
+o.filter((x) => true);
+o.on((x) => {console.log('cool!');});
+o.emit({a:12});
+
+// vim: set ts=8 sw=4 tw=0 et :
