3cf6631c9b19ca602494b7f8c28234b0e5679624
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-05-19 12:01:17 +0300

    Basic observable implementation
    - Implement methods of observable

--- LiveTable/observable.js
+++ LiveTable/observable.js
@@ -1,29 +1,30 @@
 class Observable {
     
     constructor() {
-        console.log('new Observable()');
         this.subscribers = [];
     }
 
     filter(fn) {
         const filtered = new Observable();
-        console.log(`Observable.filter(${fn});`); 
+        this.subscribers.push({fn: fn, cb: filtered.emit.bind(filtered)});
         return filtered;
     }
 
     on(callback) {
-        console.log(`Observable.on(${callback});`);
+        this.subscribers.push({fn: (x) => true, cb: callback});
     }
 
     emit(data) {
-        console.log(`Observable.emit(${data});`);
+        console.log(this);
+        this.subscribers.forEach( (s) => { if (s.fn(data)) { s.cb(data); } });
     }
 }
 
-
-var o = new Observable();
-o.filter((x) => true);
-o.on((x) => {console.log('cool!');});
-o.emit({a:12});
+// var o = new Observable();
+// var o1 = o.filter(({a}) => a < 12);
+// o.on((x) => {console.log(`o: ${x}`);});
+// o1.on((x) => {console.log(`o1: ${x}`);});
+// o.emit({a:1});
+// o.emit({a:15});
 
 // vim: set ts=8 sw=4 tw=0 et :
